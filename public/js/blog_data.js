90fc91a9a4511024db2cbf23ff67663e2b207ae5
Kevin Bongiovanni <whoppingbark@gmail.com>
2014-12-19 13:18:49 -0600

    exercises

---
+++ public/js/blog_data.js
@@ -0,0 +1,34 @@
+var data = [
+
+	{
+		"title" : "Blog Title 1",
+		"date" : "2014-12-17",
+		"author" : "Kevin Bongiovanni",
+		"content" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tincidunt accumsan magna, vel congue neque feugiat nec. Maecenas mattis mauris quis dolor vehicula, sed congue nisl mollis. Sed facilisis sem vel sapien facilisis, at scelerisque mauris tincidunt. Aenean nec nulla mi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque in est vitae nibh ullamcorper gravida nec quis orci. Fusce convallis ipsum ante, sed efficitur mi mattis venenatis. Sed tempus ultricies dui vel tempor. Suspendisse pulvinar elementum justo, ut ornare magna. Ut posuere, urna quis dignissim facilisis, lorem neque consequat massa, et interdum lacus purus ut erat. Praesent leo turpis, fermentum et rutrum in, iaculis vel mauris."
+	},
+	{
+		"title" : "Blog Title 2",
+		"date" : "2014-12-12",
+		"author" : "Kevin Bongiovanni",
+		"content" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tincidunt accumsan magna, vel congue neque feugiat nec. Maecenas mattis mauris quis dolor vehicula, sed congue nisl mollis. Sed facilisis sem vel sapien facilisis, at scelerisque mauris tincidunt. Aenean nec nulla mi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque in est vitae nibh ullamcorper gravida nec quis orci. Fusce convallis ipsum ante, sed efficitur mi mattis venenatis. Sed tempus ultricies dui vel tempor. Suspendisse pulvinar elementum justo, ut ornare magna. Ut posuere, urna quis dignissim facilisis, lorem neque consequat massa, et interdum lacus purus ut erat. Praesent leo turpis, fermentum et rutrum in, iaculis vel mauris."
+	},
+	{
+		"title" : "Blog Title3",
+		"date" : "2014-12-05",
+		"author" : "Kevin Bongiovanni",
+		"content" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tincidunt accumsan magna, vel congue neque feugiat nec. Maecenas mattis mauris quis dolor vehicula, sed congue nisl mollis. Sed facilisis sem vel sapien facilisis, at scelerisque mauris tincidunt. Aenean nec nulla mi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque in est vitae nibh ullamcorper gravida nec quis orci. Fusce convallis ipsum ante, sed efficitur mi mattis venenatis. Sed tempus ultricies dui vel tempor. Suspendisse pulvinar elementum justo, ut ornare magna. Ut posuere, urna quis dignissim facilisis, lorem neque consequat massa, et interdum lacus purus ut erat. Praesent leo turpis, fermentum et rutrum in, iaculis vel mauris."
+	},
+	{
+		"title" : "Blog Title4",
+		"date" : "2014-11-18",
+		"author" : "Kevin Bongiovanni",
+		"content" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tincidunt accumsan magna, vel congue neque feugiat nec. Maecenas mattis mauris quis dolor vehicula, sed congue nisl mollis. Sed facilisis sem vel sapien facilisis, at scelerisque mauris tincidunt. Aenean nec nulla mi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque in est vitae nibh ullamcorper gravida nec quis orci. Fusce convallis ipsum ante, sed efficitur mi mattis venenatis. Sed tempus ultricies dui vel tempor. Suspendisse pulvinar elementum justo, ut ornare magna. Ut posuere, urna quis dignissim facilisis, lorem neque consequat massa, et interdum lacus purus ut erat. Praesent leo turpis, fermentum et rutrum in, iaculis vel mauris."
+	},
+	{
+		"title" : "Blog Title5",
+		"date" : "2014-12-10",
+		"author" : "Kevin Bongiovanni",
+		"content" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tincidunt accumsan magna, vel congue neque feugiat nec. Maecenas mattis mauris quis dolor vehicula, sed congue nisl mollis. Sed facilisis sem vel sapien facilisis, at scelerisque mauris tincidunt. Aenean nec nulla mi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque in est vitae nibh ullamcorper gravida nec quis orci. Fusce convallis ipsum ante, sed efficitur mi mattis venenatis. Sed tempus ultricies dui vel tempor. Suspendisse pulvinar elementum justo, ut ornare magna. Ut posuere, urna quis dignissim facilisis, lorem neque consequat massa, et interdum lacus purus ut erat. Praesent leo turpis, fermentum et rutrum in, iaculis vel mauris."
+	}
+
+];
